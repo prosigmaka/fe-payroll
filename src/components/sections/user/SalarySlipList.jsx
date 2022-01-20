6f48a9f25c6de9e429824cf72c4465349d2899d1
dafiqarba <daviqarba@live.com>
2022-01-20 15:08:23 +0700

    Add SalarySlipList component and style

--- src/components/sections/user/SalarySlipList.jsx
+++ src/components/sections/user/SalarySlipList.jsx
@@ -1,5 +1,110 @@
+// MUI component
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+// Styled MUI Component
+import {StyledTableCell, StyledTableRow, StyledButton, Status} from './../../../styles/section/user/SalarySlipList.styled';
+// Temporary Local State
+import {useState} from 'react';
+
+function createData(paymentID, date, amount, status) {
+  return { paymentID, date, amount, status};
+}
+
+const rows = [
+  createData('183940359', '31-01-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-02-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-03-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-04-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-05-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-06-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-07-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-08-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-09-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-10-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-11-2021', '5.500.000', 'Completed'),
+  createData('183940359', '31-12-2021', '5.500.000', 'Scheduled'),
+];
+
+const getYear = () => {
+  const currentDate = new Date();
+  return String(currentDate.getFullYear());
+}
+
 const SalarySlipList = (props) => {
-  return <div>Salary SlipList</div>;
+  const [filter, setFilter] = useState('');
+  const filterHandler = (e) => {
+    setFilter(e.target.value);
+}
+  return (
+    <Container sx={{ marginTop: '3rem', marginBottom: '3rem', width: '60rem' }}>
+      <Box>
+        <Toolbar disableGutters={true} sx={{marginBottom: '1rem'}}>
+          <Typography sx={{ flex: '1 1 100%' }} variant="h3" id="tableTitle" component="div">
+            Payment History 
+          </Typography>
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="filterByYear">Filter by Year</InputLabel>
+              <Select
+                labelId="filterByYear"
+                value={filter}
+                onChange={filterHandler}
+                label="Filter by Year"
+              >
+                <MenuItem value=""><em>None</em>
+                </MenuItem>
+                <MenuItem value="2022">{getYear()}</MenuItem>
+              </Select>
+            </FormControl>
+        </Toolbar>
+        <TableContainer component={Paper} sx={{boxShadow: '0 1rem 1rem  rgba(0,0,0,0.2)'}}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Payment ID</StyledTableCell>
+                <StyledTableCell align="right">Date</StyledTableCell>
+                <StyledTableCell align="right">Amount</StyledTableCell>
+                <StyledTableCell align="right">Status</StyledTableCell>
+                <StyledTableCell align="right">Action</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.date}>
+                  <TableCell component="th" scope="row">
+                    {row.paymentID}
+                  </TableCell>
+                  <TableCell align="right">{row.date}</TableCell>
+                  <TableCell align="right">{row.amount}</TableCell>
+                  <TableCell align="right">
+                    <Status status={row.status}>{row.status}</Status>
+                  </TableCell>
+                  <TableCell align="right">
+                    <StyledButton size="small" variant="outlined">
+                      Detail
+                    </StyledButton>
+                  </TableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Container>
+  );
 };
 
 export default SalarySlipList;
