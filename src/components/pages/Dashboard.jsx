a1066842cb7405cbd8fdaac3279bb1a5f59262f8
dafiqarba <daviqarba@live.com>
2022-01-21 14:58:00 +0700

    Adjust main wrapper margin

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -4,34 +4,43 @@ import ApplicationBar from './../common/ApplicationBar';
 import SideNav from './../common/SideNav';
 import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
+import Container from '@mui/material/Container';
 
-const MainWrapper = styled('div')(`
-  padding-left: 3rem;
-  padding-top: 3rem;
-`)
+const MarginTop = styled('div')(({ theme, open }) => ({
+  ...theme.mixins.toolbar,
+}));
+
+const MainWrapper = styled('div')(({ theme, open }) => ({
+  marginLeft: `calc(${theme.spacing(6)})`,
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: `calc(${theme.spacing(7)})`,
+  },
+}));
 
 const Dashboard = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const drawerHandler = () => setIsOpen(!isOpen);
-  
 
   return (
     <>
       <Header>
         {/* ApplicationBar Component placement */}
-        <ApplicationBar status={isOpen} openDrawer={drawerHandler}/>
+        <ApplicationBar status={isOpen} openDrawer={drawerHandler} />
         {/* ApplicationBar Component placement */}
       </Header>
       <Nav>
         {/* SidebarNav Component placement */}
-        <SideNav status={isOpen} toggleDrawer={drawerHandler}/>
+        <SideNav status={isOpen} toggleDrawer={drawerHandler} />
         {/* SidebarNav Component placement */}
       </Nav>
 
       <Main>
         <MainWrapper>
-        <Outlet />
+          <MarginTop />
+          <Container sx={{ marginTop: '3rem', marginBottom: '3rem', width: '100%' }}>
+            <Outlet />
+          </Container>
         </MainWrapper>
       </Main>
 
