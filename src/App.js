05cc0cc40610c96bafdef1f546e2bc20df4b71fb
Purwa Aditya Wisnu Wardhana <83526639+adityaw24@users.noreply.github.com>
2022-01-24 17:36:29 +0700

    add dashboard summary admin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 // Page Components
-import Dashboard from './components/pages/Dashboard';
-import Login from './components/pages/Login';
+import Dashboard from "./components/pages/Dashboard";
+import Login from "./components/pages/Login";
 // Admin Section Components
-import DashboardAdmin from './components/sections/admin/DashboardAdmin';
-import AdminSummary from './components/sections/admin/AdminSummary';
-import ApprovalPanel from './components/sections/admin/ApprovalPanel';
-import PayrollPanel from './components/sections/admin/PayrollPanel';
+import DashboardAdmin from "./components/sections/admin/DashboardAdmin";
+import AdminSummary from "./components/sections/admin/AdminSummary";
+import ApprovalPanel from "./components/sections/admin/ApprovalPanel";
+import PayrollPanel from "./components/sections/admin/PayrollPanel";
 // User Section Components
-import DashboardUser from './components/sections/user/DashboardUser';
-import UserSummary from './components/sections/user/UserSummary';
-import LeaveReqForm from './components/sections/user/LeaveReqForm';
-import SalarySlipList from './components/sections/user/SalarySlipList';
-import SalarySlip from './components/sections/user/SalarySlip';
-import SickReqForm from './components/sections/user/SickReqForm';
-import PermissionReqForm from './components/sections/user/PermissionReqForm';
-import ReviewApproval from './components/sections/admin/ReviewApproval';
-
-
+import DashboardUser from "./components/sections/user/DashboardUser";
+import UserSummary from "./components/sections/user/UserSummary";
+import LeaveReqForm from "./components/sections/user/LeaveReqForm";
+import SalarySlipList from "./components/sections/user/SalarySlipList";
+import SalarySlip from "./components/sections/user/SalarySlip";
+import SickReqForm from "./components/sections/user/SickReqForm";
+import PermissionReqForm from "./components/sections/user/PermissionReqForm";
+import ReviewApproval from "./components/sections/admin/ReviewApproval";
+import SalarySlipReview from "./components/sections/admin/SalarySlipReview";
 
 function App() {
   return (
@@ -32,12 +31,13 @@ function App() {
             <Route path="summary" element={<AdminSummary />} />
             <Route path="approval-panel" element={<ApprovalPanel />} />
             <Route path="payroll-panel" element={<PayrollPanel />} />
-            <Route path="review-approval" element={<ReviewApproval/>} />
+            <Route path="review-approval" element={<ReviewApproval />} />
+            <Route path="review-salary" element={<SalarySlipReview />} />
           </Route>
           <Route path="user" element={<Navigate replace to="summary" />} />
           <Route path="user" element={<DashboardUser />}>
             <Route path="summary" element={<UserSummary />} />
-            <Route path="leave-request" element={<LeaveReqForm/>} />
+            <Route path="leave-request" element={<LeaveReqForm />} />
             <Route path="sick-request" element={<SickReqForm />} />
             <Route path="permission-request" element={<PermissionReqForm />} />
             <Route path="payroll-history" element={<SalarySlipList />} />
--- src/components/sections/admin/AdminSummary.jsx
+++ src/components/sections/admin/AdminSummary.jsx
@@ -1,5 +1,170 @@
-const AdminSummary = (props) => {
-  return <div>AdminSummary</div>;
+import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+// import { Box } from '@mui/system';
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Link } from "react-router-dom";
+import { SalaryData } from "../../../fakeDb/dataDetail";
+import { DataGrid } from "@mui/x-data-grid";
+import { DataPayroll } from "../../../fakeDb/dataGridPayroll";
+
+const linkStyle = {
+  textDecoration: "none",
+  color: "inherit",
 };
+
+const approvalItems = [
+  {
+    title: "Leave Request",
+    today: "Today",
+    countToday: "2",
+    week: "This Week",
+    countWeek: "5",
+    month: "This Month",
+    countMonth: "10",
+  },
+  {
+    title: "Leave Permit",
+    today: "Today",
+    countToday: "2",
+    week: "This Week",
+    countWeek: "5",
+    month: "This Month",
+    countMonth: "10",
+  },
+  {
+    title: "Leave Sick",
+    today: "Today",
+    countToday: "2",
+    week: "This Week",
+    countWeek: "5",
+    month: "This Month",
+    countMonth: "10",
+  },
+];
+
+const row = SalaryData.map((row) => {
+  return {
+    id: row.employeeId,
+    paymentId: row.paymentId,
+    name: row.name,
+    paymentDate: row.paymentDate,
+    totalSalary: row.totalSalary,
+    paymentStatus: row.paymentStatus,
+  };
+});
+
+function AdminSummary() {
+  return (
+    <Container mx={5}>
+      <Box pb={12}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={0}
+          mb={3}
+        >
+          <Grid item xs={12} sm>
+            <Typography variant="h5" sx={{ mb: { xs: 1, sm: 0 } }}>
+              Leave Request Today
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm sx={{ textAlign: { xs: "start", sm: "end" } }}>
+            <Button
+              variant="contained"
+              color="primary"
+              endIcon={<ChevronRightIcon />}
+              sx={{ color: "white" }}
+            >
+              <Link to="/dashboard/admiin/approval-panel" style={linkStyle}>
+                <Typography variant="button">View All</Typography>
+              </Link>
+            </Button>
+          </Grid>
+        </Grid>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="stretch"
+          sx={{ flexWrap: "wrap" }}
+        >
+          {approvalItems.map((item, index) => (
+            <Paper
+              elevation={6}
+              key={index}
+              sx={{
+                minWidth: { xs: "100%", sm: "40%", md: "28%" },
+                my: { xs: 2, sm: 1, md: 0 },
+              }}
+            >
+              <Card>
+                <CardContent>
+                  <Typography variant="h6" component="div" mb={2}>
+                    {item.title}
+                  </Typography>
+                  <Typography sx={{ fontSize: "3rem" }}>
+                    {item.countToday}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Paper>
+          ))}
+        </Stack>
+      </Box>
+      <Box>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={0}
+          mb={3}
+        >
+          <Grid item xs={12} sm>
+            <Typography variant="h5" sx={{ mb: { xs: 1, sm: 0 } }}>
+              Payroll Panel
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm sx={{ textAlign: { xs: "start", sm: "end" } }}>
+            <Button
+              variant="contained"
+              color="primary"
+              endIcon={<ChevronRightIcon />}
+              sx={{ color: "white" }}
+            >
+              <Link to="/dashboard/admin/payroll-panel" style={linkStyle}>
+                <Typography variant="button">View All</Typography>
+              </Link>
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+      <DataGrid
+        rows={row}
+        columns={DataPayroll.columns}
+        pageSize={5}
+        rowCount={5}
+        autoHeight={true}
+        autoPageSize={true}
+        hideFooterPagination={true}
+        sx={{
+          backgroundColor: "white",
+          boxShadow: "0 1rem 1rem  rgba(0,0,0,0.2)",
+        }}
+      />
+    </Container>
+  );
+}
 
 export default AdminSummary;
