46ea2e04b2fbbb73848f96538fbc4949995228af
Purwa Aditya Wisnu Wardhana <83526639+adityaw24@users.noreply.github.com>
2022-01-24 14:21:07 +0700

    edit header

--- src/components/common/ApplicationBar.jsx
+++ src/components/common/ApplicationBar.jsx
@@ -1,25 +1,25 @@
-import * as React from 'react';
-import { styled} from '@mui/material/styles';
-import MuiAppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import LogoutIcon from '@mui/icons-material/Logout';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import MuiAppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import LogoutIcon from "@mui/icons-material/Logout";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
 
 const drawerWidth = 240;
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer - 1,
   backgroundColor: theme.palette.common.neutral_white_medium,
   marginLeft: `calc(${theme.spacing(7)} + 1px)`,
   width: `calc(100% - ${theme.spacing(7)} + 1px)`,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
@@ -27,17 +27,16 @@ const AppBar = styled(MuiAppBar, {
     marginLeft: drawerWidth,
     zIndex: theme.zIndex.drawer - 1,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-
 const ApplicationBar = (props) => {
-    // const openHandler = () => props.openDrawer(true);
-    return (
+  // const openHandler = () => props.openDrawer(true);
+  return (
     <Box sx={{ flexGrow: 1 }}>
       {/* <AppBar position="fixed" open={props.status} sx={{boxShadow: 'none'}}> */}
       <AppBar position="fixed" open={props.status}>
@@ -56,14 +55,14 @@ const ApplicationBar = (props) => {
             <ChevronLeftIcon />
           </IconButton> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Content Title (Temp)
+            Payroll Management System
           </Typography>
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
               color="inherit"
-              sx={{marginRight: '1rem'}}
+              sx={{ marginRight: "1rem" }}
             >
               <Badge badgeContent={17} color="error">
                 <NotificationsIcon />
@@ -78,10 +77,10 @@ const ApplicationBar = (props) => {
               //onClick={handleProfileMenuOpen}
               color="inherit"
             >
-              <LogoutIcon/>
+              <LogoutIcon />
             </IconButton>
           </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="show more"
@@ -93,11 +92,10 @@ const ApplicationBar = (props) => {
               <MoreIcon />
             </IconButton>
           </Box>
-       
         </Toolbar>
       </AppBar>
     </Box>
   );
-}
+};
 
 export default ApplicationBar;
