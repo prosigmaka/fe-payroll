02a8a21e23ecaa701e880650aeba78957b199cb0
Purwa Aditya Wisnu Wardhana <83526639+adityaw24@users.noreply.github.com>
2022-01-24 17:36:10 +0700

    fix column data payroll panel

--- src/components/sections/admin/PayrollPanel.jsx
+++ src/components/sections/admin/PayrollPanel.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import {
   DataGrid,
-  // GridActionsCellItem,
   gridClasses,
   GridToolbarContainer,
   GridToolbarExport,
@@ -10,58 +9,7 @@ import { Box } from "@mui/system";
 import { Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { SalaryData } from "../../../fakeDb/dataDetail";
-
-const columns = [
-  { field: "id", headerName: "Employee ID", width: 180, type: "number" },
-  {
-    field: "paymentId",
-    headerName: "Payment ID",
-    width: 180,
-    type: "number",
-  },
-  { field: "name", headerName: "Name", width: 250, type: "string" },
-  {
-    field: "paymentDate",
-    headerName: "Date",
-    width: 180,
-    type: "date",
-    editable: true,
-  },
-  {
-    field: "totalSalary",
-    headerName: "Amount",
-    width: 200,
-    type: "number",
-    editable: true,
-  },
-  {
-    field: "paymentStatus",
-    headerName: "Status",
-    width: 150,
-    type: "string",
-    editable: true,
-  },
-  // {
-  //   field: "link",
-  //   headerName: "Link",
-  //   hide: true,
-  // },
-  // {
-  //   field: "actions",
-  //   type: "actions",
-  //   width: 80,
-  //   getActions: (item) => [
-  //     <GridActionsCellItem
-  //       //   icon={<FileCopyIcon />}
-  //       label="Details"
-  //       onClick={() => {
-  //         window.open("/salary-slip", "_blank");
-  //       }}
-  //       showInMenu
-  //     />,
-  //   ],
-  // },
-];
+import { DataPayroll } from "../../../fakeDb/dataGridPayroll";
 
 const row = SalaryData.map((row) => {
   return {
@@ -115,15 +63,15 @@ function PayrollPanel(props) {
     >
       <div
         style={{
-          maxHeight: 500,
+          maxHeight: "100%",
           width: "100%",
           margin: "auto",
-          overflow: "hidden",
+          overflow: "auto",
         }}
       >
         <DataGrid
           rows={row}
-          columns={columns}
+          columns={DataPayroll.columns}
           pageSize={pageSize}
           onPageSizeChange={(newPage) => setPageSize(newPage)}
           autoHeight={true}
