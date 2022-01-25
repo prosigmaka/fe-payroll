dd1015a0256fdff7bd638fbf7a660fdf1eb40328
Purwa Aditya Wisnu Wardhana <83526639+adityaw24@users.noreply.github.com>
2022-01-25 16:38:35 +0700

    add function export to excel

---
+++ src/hooks/exportToExcel.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
+import { Button } from "@mui/material";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+
+export const ExportToExcel = ({ apiData, fileName }) => {
+  const fileType =
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+  const fileExtension = ".xlsx";
+
+  const exportToCSV = (apiData, fileName) => {
+    const ws = XLSX.utils.json_to_sheet(apiData);
+    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
+  };
+
+  return (
+    <Button
+      variant="contained"
+      startIcon={<FileDownloadIcon />}
+      size="small"
+      onClick={(e) => exportToCSV(apiData, fileName)}
+      sx={{ color: "white" }}
+    >
+      Export
+    </Button>
+  );
+};
